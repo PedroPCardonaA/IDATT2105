311931fa0c630431d475d25b56292fc2ef31962b
Anders Austlid <andemau@ntnu.no>
2023-03-18 13:56:10 +0100

    Initial commit, Spring BOOT Web starter

---
+++ src/main/java/ntnu/idi/idatt2015/tokenly/backend/TokenlyBackendApplication.java
@@ -0,0 +1,13 @@
+package ntnu.idi.idatt2015.tokenly.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenlyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenlyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ntnu/idi/idatt2015/tokenly/backend/TokenlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ntnu.idi.idatt2015.tokenly.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenlyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
