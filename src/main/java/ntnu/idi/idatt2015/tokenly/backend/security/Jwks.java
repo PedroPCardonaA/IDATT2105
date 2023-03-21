84d1edf2e88e9f0e2cb94e8bb3169863b112f35f
Anders Austlid <andemau@ntnu.no>
2023-03-21 15:42:02 +0100

    Added some JavaDoc for security utility classes

--- src/main/java/ntnu/idi/idatt2015/tokenly/backend/security/Jwks.java
+++ src/main/java/ntnu/idi/idatt2015/tokenly/backend/security/Jwks.java
@@ -1,5 +1,8 @@
+/**
+ * ntnu.idi.idatt2105.tokenly.backend.security
+ * Provides classes related to security functionalities in the application
+ */
 package ntnu.idi.idatt2015.tokenly.backend.security;
-
 
 import com.nimbusds.jose.jwk.RSAKey;
 import java.security.KeyPair;
@@ -7,10 +10,24 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.util.UUID;
 
+/**
+ * Jwks is a utility class for generating RSA keys for JSON Web Key Set (JWKS) support.
+ * It provides a method to generate an RSA key pair and create an RSAKey object.
+ *
+ * <p>This class cannot be instantiated.</p>
+ */
 public class Jwks {
 
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
     private Jwks() {}
 
+    /**
+     * Generates an RSA key pair, creates an RSAKey object, and assigns a unique key ID to it.
+     *
+     * @return An RSAKey object with a public key, private key, and a unique key ID.
+     */
     public static RSAKey generateRsa() {
         KeyPair keyPair = KeyGeneratorUtils.generateRsaKey();
         RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
