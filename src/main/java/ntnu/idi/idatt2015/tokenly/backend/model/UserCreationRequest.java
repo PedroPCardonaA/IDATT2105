c0038b93ea145a0fdc4efa3398a5b707e0c54b2a
Anders Austlid <andemau@ntnu.no>
2023-03-22 15:22:53 +0100

    API endpoints and auth whitelist didn't match

--- src/main/java/ntnu/idi/idatt2015/tokenly/backend/model/UserCreationRequest.java
+++ src/main/java/ntnu/idi/idatt2015/tokenly/backend/model/UserCreationRequest.java
@@ -7,4 +7,5 @@ package ntnu.idi.idatt2015.tokenly.backend.model;
 /**
  * UserCreationRequest is a record class representing a user creation request containing a username and password.
  */
-public record UserCreationRequest(String username, String password) { }
+public record UserCreationRequest(String username, String password,
+                                  String firstName, String lastName, String email) { }
