7866a93511e9139201675d24d2aed7bc77df4197
Pedro Cardona <pedropca@stud.ntnu.no>
2023-03-22 11:52:59 +0100

    TransactionRepository and JdbcTransactionRepository classes were added

--- src/main/java/ntnu/idi/idatt2015/tokenly/backend/repository/TransactionRepository.java
+++ src/main/java/ntnu/idi/idatt2015/tokenly/backend/repository/TransactionRepository.java
@@ -3,10 +3,11 @@ package ntnu.idi.idatt2015.tokenly.backend.repository;
 import ntnu.idi.idatt2015.tokenly.backend.model.Transaction;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TransactionRepository {
     void save (Transaction transaction);
-    List<Transaction> getAllTransactions();
-    List<Transaction> getAllTransactionBySellerId(long sellerId);
-    List<Transaction> getAllTransactionByBuyerId(long buyerId);
+    Optional<List<Transaction>> getAllTransactions();
+    Optional<List<Transaction>> getAllTransactionBySellerName(String sellerName);
+    Optional<List<Transaction>> getAllTransactionByBuyerName(String buyerName);
 }
