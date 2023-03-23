98a81d36d8d481371aaf942cba57f97882e3f1db
Anders Austlid <andemau@ntnu.no>
2023-03-23 12:57:48 +0100

    UserCreation now handles user profile data

--- src/main/java/ntnu/idi/idatt2015/tokenly/backend/model/UserCreationRequest.java
+++ src/main/java/ntnu/idi/idatt2015/tokenly/backend/model/UserCreationRequest.java
@@ -4,8 +4,11 @@
  */
 package ntnu.idi.idatt2015.tokenly.backend.model;
 
+import java.sql.Date;
+
 /**
  * UserCreationRequest is a record class representing a user creation request containing a username and password.
  */
 public record UserCreationRequest(String username, String password,
-                                  String firstName, String lastName, String email) { }
+                                  String firstName, String lastName, String email,
+                                  Date birthdate) { }
