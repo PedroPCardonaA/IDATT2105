ea9cfab0d24359f7b784b73bb21a44c4bcb5a945
Anders Austlid <andemau@ntnu.no>
2023-03-24 14:42:07 +0100

    Added wishlist controller

--- src/main/java/ntnu/idi/idatt2015/tokenly/backend/JDBCrepository/JdbcWishListRepository.java
+++ src/main/java/ntnu/idi/idatt2015/tokenly/backend/JDBCrepository/JdbcWishListRepository.java
@@ -2,11 +2,15 @@ package ntnu.idi.idatt2015.tokenly.backend.JDBCrepository;
 
 import ntnu.idi.idatt2015.tokenly.backend.model.Item;
 import ntnu.idi.idatt2015.tokenly.backend.model.User;
+import ntnu.idi.idatt2015.tokenly.backend.model.Wishlist;
 import ntnu.idi.idatt2015.tokenly.backend.repository.WishListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
@@ -38,12 +42,25 @@ public class JdbcWishListRepository implements WishListRepository {
     /**
      * save method to save user's wish list
      *
-     * @param userName long value representing the user id
-     * @param itemId long value representing the item id
+     * @param wishlist A Wishlist object to save
+     * @return The created Wishlist object
      */
     @Override
-    public void save(String userName, long itemId) {
+    public Wishlist save(Wishlist wishlist) {
+        String sql = "INSERT INTO WISH_LIST(USERNAME, ITEM_ID) VALUES (:username, :itemId)";
 
+        HashMap<String,Object> params = new HashMap<>();
+        params.put("username",wishlist.username());
+        params.put("itemId",wishlist.itemId());
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        try {
+            namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(params), keyHolder, new String[]{"username","item_id"});
+            return wishlist;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     /**
@@ -53,15 +70,16 @@ public class JdbcWishListRepository implements WishListRepository {
      * @return Optional object containing the list of users
      */
     @Override
-    public Optional<List<User>> getAllUserThatWantTheItem(long itemId) {
-        String sql = "SELECT * FROM USER WHERE USERNAME IN(SELECT USERNAME " +
+    public Optional<List<String>> getAllUserThatWantTheItem(long itemId) {
+        String sql = "SELECT USERNAME FROM USERS WHERE USERNAME IN(SELECT USERNAME " +
                 "FROM WISH_LIST WHERE ITEM_ID = :itemId)";
         HashMap<String,Object> params = new HashMap<>();
         params.put("itemId", itemId);
         try {
-            List<User> users = namedParameterJdbcTemplate.query(sql,params,new BeanPropertyRowMapper<>(User.class));
-            return Optional.of(users);
+            List<String> usernames = namedParameterJdbcTemplate.query(sql,params, (rs, rowNum) -> rs.getString("USERNAME"));
+            return Optional.of(usernames);
         }catch (Exception e){
+            System.out.println(e.getMessage());
             return Optional.empty();
         }
     }
---
+++ src/main/java/ntnu/idi/idatt2015/tokenly/backend/model/Wishlist.java
@@ -0,0 +1,4 @@
+package ntnu.idi.idatt2015.tokenly.backend.model;
+
+public record Wishlist(String username, long itemId) {
+}
