e040031e06a01945cc933fa8a06fda8e99c821a2
Anders Austlid <andemau@ntnu.no>
2023-03-25 16:18:33 +0100

    Final service JavaDoc added

--- src/main/java/ntnu/idi/idatt2015/tokenly/backend/service/TokenService.java
+++ src/main/java/ntnu/idi/idatt2015/tokenly/backend/service/TokenService.java
@@ -11,15 +11,34 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.stream.Collectors;
 
+/**
+ * This class is used to generate a JWT token.
+ * @author tokenly-team
+ * @version 1.0
+ * @since 22.03.2023
+ */
 @Service
 public class TokenService {
 
     private final JwtEncoder encoder;
 
+    /**
+     * Creates a new TokenService object.
+     * The encoder object is injected by Spring.
+     *
+     * @param encoder the JwtEncoder object to use
+     */
     public TokenService(JwtEncoder encoder) {
         this.encoder = encoder;
     }
 
+    /**
+     * Generates a JWT token from an Authentication object.
+     * Generated tokens are self-signed and expire after one hour.
+     *
+     * @param authentication the Authentication object to generate the token from
+     * @return the generated token
+     */
     public String generateToken(Authentication authentication) {
         Instant now = Instant.now();
         String scope = authentication.getAuthorities().stream()
