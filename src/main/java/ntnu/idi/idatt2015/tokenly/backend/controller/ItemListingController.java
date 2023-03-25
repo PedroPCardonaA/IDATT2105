6c3d3a28c0cbef2816fe4f07144cf4dbef752b26
Pedro Cardona <pedropca@stud.ntnu.no>
2023-03-25 14:48:58 +0100

    Some code

--- src/main/java/ntnu/idi/idatt2015/tokenly/backend/controller/ItemListingController.java
+++ src/main/java/ntnu/idi/idatt2015/tokenly/backend/controller/ItemListingController.java
@@ -11,6 +11,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * The ItemListingController class is a REST controller that handles requests related to item listings.
+ *
+ * @author tokenly-team
+ * @version 1.0
+ * @since 22.03.2023
+ */
 @RestController
 @RequestMapping("/api/itemListing")
 @CrossOrigin
@@ -21,6 +28,16 @@ public class ItemListingController {
      @Autowired
     CategoryRepository categoryRepository;
 
+    /**
+     * Retrieves all item listings from the database.
+     *
+     * @param page the page number of the result set to retrieve
+     * @param size the maximum number of item listings to retrieve
+     * @param sortBy the attribute to sort the result set by
+     * @param order the order in which to sort the result set (ASC or DESC)
+     *
+     * @return a ResponseEntity containing a list of item listings, or a BAD_REQUEST response if none were found
+     */
      @GetMapping("/")
     public ResponseEntity<?> getAllItemsListing(@RequestParam (value="page", defaultValue ="0") int page,
                                                 @RequestParam(value = "size", defaultValue = "12") int size,
@@ -39,6 +56,17 @@ public class ItemListingController {
          }
      }
 
+    /**
+     * Retrieves all item listings with the specified category from the database.
+     *
+     * @param page the page number of the result set to retrieve
+     * @param size the maximum number of item listings to retrieve
+     * @param sortBy the attribute to sort the result set by
+     * @param order the order in which to sort the result set (ASC or DESC)
+     * @param category the category to filter the result set by
+     *
+     * @return a ResponseEntity containing a list of item listings with the specified category, or a BAD_REQUEST response if none were found
+     */
     @GetMapping("/category")
     public ResponseEntity<?> getAllItemsListingByCategory(@RequestParam (value="page", defaultValue ="0") int page,
                                                 @RequestParam(value = "size", defaultValue = "12") int size,
--- src/main/java/ntnu/idi/idatt2015/tokenly/backend/service/ControlInputService.java
+++ src/main/java/ntnu/idi/idatt2015/tokenly/backend/service/ControlInputService.java
@@ -1,17 +1,8 @@
 package ntnu.idi.idatt2015.tokenly.backend.service;
 
-import ntnu.idi.idatt2015.tokenly.backend.JDBCrepository.JdbcCategoryRepository;
-import ntnu.idi.idatt2015.tokenly.backend.model.Category;
-import ntnu.idi.idatt2015.tokenly.backend.repository.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-
 import java.util.Arrays;
-import java.util.List;
 
 public class ControlInputService {
-
-    static JdbcCategoryRepository jdbcCategoryRepository = new JdbcCategoryRepository(new JdbcTemplate());
     private static String[] orders = {"ASC","DESC"};
     private static String[] tableNamesItemListing = {"item_Id","item_name","owner_name","description", "listing_id","min_price","max_price","publication_time","visits"};
 
