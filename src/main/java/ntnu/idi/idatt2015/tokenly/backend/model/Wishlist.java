0cd00a778565d1f2972a72cfc0931383b1337a88
Anders Austlid <andemau@ntnu.no>
2023-03-25 16:07:38 +0100

    Added some model class Javadoc

--- src/main/java/ntnu/idi/idatt2015/tokenly/backend/model/Wishlist.java
+++ src/main/java/ntnu/idi/idatt2015/tokenly/backend/model/Wishlist.java
@@ -1,4 +1,11 @@
 package ntnu.idi.idatt2015.tokenly.backend.model;
 
+/**
+ * Wishlist is a record class representing a wishlist containing a username and item id.
+ *
+ * @author tokenly-team
+ * @version 1.0
+ * @since 22.03.2023
+ */
 public record Wishlist(String username, long itemId) {
 }
