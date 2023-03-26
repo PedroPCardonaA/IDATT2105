22702ac1da6dfd253c61ff92e4a70a3ebc4cc4ad
Anders Austlid <andemau@ntnu.no>
2023-03-26 11:33:21 +0200

    Added tests for BidController

--- src/test/java/ntnu/idi/idatt2015/tokenly/backend/controller/AuthControllerTest.java
+++ src/test/java/ntnu/idi/idatt2015/tokenly/backend/controller/AuthControllerTest.java
@@ -23,7 +23,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
